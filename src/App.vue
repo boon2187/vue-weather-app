f5a918b6af2cae27b7204803ab273b5ecd50c895
boon2187 <fumiya.mai.ito@gmail.com>
2022-12-09 13:59:57 +0900

    最初のコミット

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- // App.vue -->
+
+<template>
+  <Title />
+  <Form @submit-form="getWeather" />
+</template>
+
+<script setup>
+import axios from 'axios';
+import Title from './components/Title.vue';
+import Form from './components/Form.vue';
+
+const getWeather = () => {
+  axios.get("http://api.weatherapi.com/v1/current.json?key=0be11468eae249b0b82234208221507&q=London&aqi=no")
+    .then(res => console.log(res));
+}
+</script>
---
+++ src/components/Form.vue
@@ -0,0 +1,23 @@
+<!-- Form.vue -->
+
+<template>
+    <form @submit.prevent="submitForm">
+        <input type="text" v-model="city" placeholder="都市を英語で入力" />
+        <button type="submit" >Get Weather</button>
+    </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const city = ref("");
+
+const submitForm = () => {
+    emits("submit-form");
+}
+
+// 上位コンポーネント（App.vue）にエミットする
+const emits = defineEmits([
+    "submit-form"
+])
+</script>
