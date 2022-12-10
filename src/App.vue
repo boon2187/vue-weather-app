1a6ff5f6b182fced61c847e39f5d17851a4474b9
boon2187 <fumiya.mai.ito@gmail.com>
2022-12-10 12:32:23 +0900

    結果をresultsに保存して、Resultsコンポーネントに渡す

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,32 @@
 <template>
   <Title />
   <Form @submit-form="getWeather" />
+  <Results :results="results" />
 </template>
 
 <script setup>
+import { reactive } from 'vue';
 import axios from 'axios';
 import Title from './components/Title.vue';
 import Form from './components/Form.vue';
+import Results from './components/Results.vue';
+
+const results = reactive({
+  country: "",
+  cityName: "",
+  temperature: "",
+  conditionText: "",
+  icon: ""
+})
 
 const getWeather = () => {
   axios.get("http://api.weatherapi.com/v1/current.json?key=0be11468eae249b0b82234208221507&q=London&aqi=no")
-    .then(res => console.log(res));
+    .then(res => {
+      results.country = res.data.location.country,
+      results.cityName = res.data.location.name,
+      results.temperature = res.data.current.temp_c,
+      results.conditionText = res.data.current.condition.text,
+      results.icon = res.data.current.condition.icon
+    });
 }
 </script>
